d7f8be062ef29668b3433ad414904f1abf1ea1b7
youthtide <80157195+youthtide@users.noreply.github.com>
2023-04-06 01:15:00 +0100

    task test commit 1

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ def main_function():
     age = int(input("what's your age?"))
     if age < 18:
         print("sowwy:(")
+
+
+import random
+
+
+def test_function():
+    flip = random.random()
+    if flip < 0.5:
+        
