46544957770513de15950d8608de805206e5306f
youthtide <80157195+youthtide@users.noreply.github.com>
2023-04-06 01:26:52 +0100

    Update py_charm.py

---
+++ py_charm.py
@@ -0,0 +1,13 @@
+def main_function():
+    age = int(input("what's your age?"))
+    if age < 18:
+        print("sowwy:(")
+    else:
+        print("yay!")
+
+
+import random
+
+
+def test_function():
+    flip = random.random()
